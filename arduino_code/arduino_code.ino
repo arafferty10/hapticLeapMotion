4dd78c38095e08bf2cdeb32cf3ceb486e8ffa6f9
arafferty10 <aira5953@colorado.edu>
2018-12-13 09:48:09 -0700

    vibe motors on PWM pins

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -11,11 +11,11 @@ void setup()
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
-    pinMode(motorPin1,OUTPUT);
-    pinMode(motorPin2,OUTPUT);
-    pinMode(motorPin3,OUTPUT);
-    pinMode(motorPin4,OUTPUT);
-    pinMode(motorPin5,OUTPUT);
+    pinMode(3,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(9,OUTPUT);
+    pinMode(10,OUTPUT);
 }
 
 void loop()
@@ -32,19 +32,19 @@ void loop()
     {
         case 'A':
             digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-            digitalWrite(motorPin1, HIGH);
-            digitalWrite(motorPin2, HIGH);
-            digitalWrite(motorPin3, HIGH);
-            digitalWrite(motorPin4, HIGH);
-            digitalWrite(motorPin5, HIGH);
+            digitalWrite(3, HIGH);
+            digitalWrite(5, HIGH);
+            digitalWrite(6, HIGH);
+            digitalWrite(9, HIGH);
+            digitalWrite(10, HIGH);
             break;
         case 'Z':
             digitalWrite(LED_BUILTIN, LOW);
-            digitalWrite(motorPin1, LOW);
-            digitalWrite(motorPin2, LOW);
-            digitalWrite(motorPin3, LOW);
-            digitalWrite(motorPin4, LOW);
-            digitalWrite(motorPin5, LOW);
+            digitalWrite(3, LOW);
+            digitalWrite(5, LOW);
+            digitalWrite(6, LOW);
+            digitalWrite(9, LOW);
+            digitalWrite(10, LOW);
             break;
     }
 }
