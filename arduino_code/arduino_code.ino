4fb4e996ac9b6c153ee8bbdb426bfd7f63ffa48f
arafferty10 <aira5953@colorado.edu>
2018-12-13 12:26:48 -0700

    working arduino haptic

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -1,21 +1,21 @@
 unsigned long last_time = 0;
 
-const int motorPin1 = 1;
-const int motorPin2 = 2;
-const int motorPin3 = 3;
-const int motorPin4 = 4;
-const int motorPin5 = 5;
+const int motorPin1 = 4;
+const int motorPin2 = 5;
+const int motorPin3 = 6;
+//const int motorPin4 = 4;
+//const int motorPin5 = 5;
 
 void setup()
 {
     Serial.begin(9600);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
-    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
     pinMode(5,OUTPUT);
     pinMode(6,OUTPUT);
-    pinMode(9,OUTPUT);
-    pinMode(10,OUTPUT);
+//    pinMode(9,OUTPUT);
+//    pinMode(10,OUTPUT);
 }
 
 void loop()
@@ -32,19 +32,19 @@ void loop()
     {
         case 'A':
             digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-            digitalWrite(3, HIGH);
-            digitalWrite(5, HIGH);
-            digitalWrite(6, HIGH);
-            digitalWrite(9, HIGH);
-            digitalWrite(10, HIGH);
+            digitalWrite(motorPin1, HIGH);
+            digitalWrite(motorPin2, HIGH);
+            digitalWrite(motorPin3, HIGH);
+//            digitalWrite(9, HIGH);
+//            digitalWrite(10, HIGH);
             break;
         case 'Z':
             digitalWrite(LED_BUILTIN, LOW);
-            digitalWrite(3, LOW);
-            digitalWrite(5, LOW);
-            digitalWrite(6, LOW);
-            digitalWrite(9, LOW);
-            digitalWrite(10, LOW);
+            digitalWrite(motorPin1, LOW);
+            digitalWrite(motorPin2, LOW);
+            digitalWrite(motorPin3, LOW);
+//            digitalWrite(9, LOW);
+//            digitalWrite(10, LOW);
             break;
     }
 }
